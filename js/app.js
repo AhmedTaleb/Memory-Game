a02c0a8e1e11a804040c2aa04ba2ff10e10d1dd4
Ahmed Taleb <ahmed_taleb09@yahoo.fr>
2019-02-19 17:07:55 -0600

    Open and compare two cards at a time

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const cardsHtml = ['<i class="fa fa-diamond"></i>','<i class="fa fa-paper-plane-
                     '<i class="fa fa-leaf"></i>','<i class="fa fa-bicycle"></i>','<i class="fa fa-bomb"></i>','<i class="fa fa-bomb"></i>'];
 
 const cards = document.querySelectorAll('.card');
+let openCards = [];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -28,6 +29,13 @@ const shuffledcards = shuffle(cardsHtml);
 for (let i = 0; i < cards.length; i++) {
 
     cards[i].innerHTML = shuffledcards[i];
+    //show the card
+    cards[i].classList.add('show');
+
+    setTimeout(function(){
+    //show the cards for 1 second
+    cards[i].classList.remove('show');
+    },1000);
 }
 }
 
@@ -36,6 +44,43 @@ for (let i = 0; i < cards.length; i++) {
 function game(){
 
     addShuffledCards();
+    cards.forEach(card => {
+
+        card.addEventListener('click',function(){
+
+            if(card.classList.length == 1){
+                //open two cards at a time
+                if(openCards.length < 2){
+                    //display card
+                    card.classList.add('open','show');
+                    //add card to open cards array
+                    openCards.push(card);
+
+                    }
+                    //two cards are open
+                    if(openCards.length == 2){
+                    //compare cards
+                    if(openCards[0].innerHTML === openCards[1].innerHTML){
+                        //cards did match
+                        openCards.forEach(openCard => {
+                            openCard.classList.add('match');
+                        });
+
+                        openCards = [];
+                    }else {
+                        //cards did not match
+                        setTimeout(function(){
+                            //
+                            card.classList.remove('match','open','show');
+                            openCards[0].classList.remove('match','open','show');
+                            openCards = [];
+                            },500);
+
+                    }
+                    }
+            }
+        });
+    });
 }
 
 
